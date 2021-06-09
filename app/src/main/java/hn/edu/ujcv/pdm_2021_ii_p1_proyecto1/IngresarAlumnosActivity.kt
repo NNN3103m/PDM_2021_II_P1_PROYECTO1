40a0341ef68f6db743cecb20f024fa92da46813c
nnn31 <nnn3103m@gmail.com>
2021-06-09 17:27:08 -0600

    Validacion de vacios en IngresarAlumnosActivity.kt

--- app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p1_proyecto1/IngresarAlumnosActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p1_proyecto1/IngresarAlumnosActivity.kt
@@ -1,8 +1,11 @@
 package hn.edu.ujcv.pdm_2021_ii_p1_proyecto1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_ingresar_alumnos.*
+import kotlinx.android.synthetic.main.activity_ingresar_alumnos.view.*
 import kotlinx.android.synthetic.main.activity_visualizar_prestamos_libros_numero_cuenta.*
 
 class IngresarAlumnosActivity : AppCompatActivity() {
@@ -11,6 +14,20 @@ class IngresarAlumnosActivity : AppCompatActivity() {
         setContentView(R.layout.activity_ingresar_alumnos)
         //Botones de atras y aceptar
         btnAtrasIA.setOnClickListener{onBackPressed()}
-
+        btnAceptarIA.setOnClickListener{comprobarVacios()}
+    }
+    fun comprobarVacios(){
+        when{
+            txtNumeroCuenta.text.isEmpty()  -> Toast.makeText(this,"Debe ingresar su numero de cuenta", Toast.LENGTH_SHORT).show()
+            txtNombreAlumno.text.isEmpty()  -> Toast.makeText(this,"Debe ingresar su nombre completo", Toast.LENGTH_SHORT).show()
+            txtCarrera.text.isEmpty()       -> Toast.makeText(this,"Debe ingresar la carrera que esta cursando", Toast.LENGTH_SHORT).show()
+            txtFechaIngreso.text.isEmpty()  -> Toast.makeText(this,"Debe ingresar la fecha", Toast.LENGTH_SHORT).show()
+            txtCorreo.text.isEmpty()        -> Toast.makeText(this,"Debe ingresar su correo", Toast.LENGTH_SHORT).show()
+            else -> {
+                val intent = Intent(this, FinalizadoActivity::class.java)
+                //ntent.putExtra("nombre",txtNombreAlumno.text.toString())
+                startActivity(intent)
+            }
+        }
     }
 }
